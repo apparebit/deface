b6cea23844fe4d790f617955945d78e40e2b846c
Robert Grimm <apparebit@gmail.com>
2021-08-30 00:06:51 -0400

    Move JSON-parsing into own module; replace JsonData with Validator
    Validator has a much simpler interface, dispensing with the context manager functionality and returning a new instance from the index operator. That makes it much easier to statically type Validator. It also simplifies list processing. And the result of the index operator is consistent with developer expectations. In short, a much improved wrapper class.

---
+++ deface/jsonio.py
@@ -0,0 +1,45 @@
+# Copyright 2021 Robert Grimm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import re
+
+from binascii import unhexlify
+from typing import Union
+
+JsonT = Union[None, bool, int, float, str, ]
+
+_BROKEN_ESCAPE = re.compile(rb'\\u00([0-9a-f][0-9a-f])', re.I)
+
+def restore_utf8(data: bytes) -> bytes:
+  """
+  Restore the UTF-8 encoding for files exported from Facebook. Such files may
+  appear to be valid JSON at first but nonetheless encode all non-ASCII
+  characters incorrectly. That is the result of Facebook first generating JSON
+  text in UTF-8 with only double-quotes and newlines escaped (as ``\\"`` and
+  ``\\n``, respectively) and thereafter encoding all bytes that aren't also
+  valid ASCII as faux unicode escapes of the form ``\\u00xx``. This function
+  undoes that second faulty encoding step. Its result can safely be parsed as
+  UTF-8-encoded JSON text.
+  """
+  return re.sub(_BROKEN_ESCAPE, lambda match: unhexlify(match.group(1)), data)
+
+def read_json(path: str) -> JsonT:
+  """
+  Read the file with the given path as a JSON file with personal data exported
+  from Facebook. This function undoes Facebook's faulty encoding and then parses
+  the JSON.
+  """
+  with open(path, 'rb') as file:
+    return json.loads(restore_utf8(file.read()))
---
+++ deface/validator.py
@@ -0,0 +1,192 @@
+# Copyright 2021 Robert Grimm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import json
+
+from collections.abc import Iterator
+from typing import Any, cast, Generic, NoReturn, Optional, TypeVar, Union
+from deface.error import ValidationError
+
+KeyT = Union[int, str]
+T = TypeVar('T')
+
+class Validator(Generic[T]):
+  def __init__(
+    self,
+    value: T,
+    filename: str = '',
+    key: Optional[KeyT] = None,
+    parent: Optional[Validator[T]] = None,
+  ):
+    """
+    Create a new validator instance with the given key and value. When directly
+    creating the root validator for a JSON document, provide the ``value`` and
+    possibly ``filename`` as arguments. The latter improves error reporting.
+    When indirectly creating a new child validator through an indexing operator,
+    the ``parent`` and ``key`` can't be ``None``::
+
+      parent._value[key] == value
+
+    This equality holds for all derived validators but not for the root
+    validator (whose parent is itself).
+    """
+    self._filename: str = parent._filename if parent is not None else filename
+    self._key: KeyT = key if key is not None else ''
+    self._value: T = value
+    self._parent: Validator[T] = parent if parent is not None else self
+
+  @property
+  def filename(self) -> str:
+    """Get the filename for the file with the JSON data."""
+    return self._filename
+
+  @property
+  def only_key(self) -> str:
+    """
+    Get the only key. If the current value is a singleton object, this method
+    returns the only key. Otherwise, it raises an assertion error.
+    """
+    value = self._value
+    assert isinstance(value, dict)
+    keys = value.keys()
+    assert len(keys) == 1
+    return next(iter(keys))
+
+  @property
+  def keypath(self) -> str:
+    """
+    Determine the key path for this validator value. The key path is composed
+    from list items, formatted as say ``[42]``, and object fields, formatted as
+    ``.answer`` for fields named with Python identifiers or ``["42"]``
+    otherwise.
+    """
+    def format(key: KeyT) -> str:
+      if isinstance(key, str) and key.isidentifier():
+        return f'.{key}'
+      return f'[{json.dumps(key)}]'
+
+    path = []
+    current = self
+    while current != current._parent:
+      path.append(format(current._key))
+      current = current._parent
+    path.reverse()
+    return ''.join(path)
+
+  @property
+  def value(self) -> T:
+    """Get the current value."""
+    return self._value
+
+  def raise_invalid(self, message: str) -> NoReturn:
+    """
+    Raise a validation error for the current value. The error message is
+    automatically formatted as the character sequence consisting of filename,
+    keypath, a space, and the given message string.
+    """
+    keypath = self.keypath
+    raise ValidationError(f'{self._filename}{keypath} {message}')
+
+  def to_integer(self) -> Validator[int]:
+    """Coerce the current value to an integer."""
+    if not isinstance(self._value, int):
+      self.raise_invalid('is not an integer')
+    return cast(Validator[int], self)
+
+  def to_number(self) -> Validator[float]:
+    """Coerce the current value to an integral or floating point number."""
+    if not isinstance(self._value, (int, float)):
+      self.raise_invalid('is not an integer nor a float')
+    return cast(Validator[float], self)
+
+  def to_string(self) -> Validator[str]:
+    """Coerce the current value to a string."""
+    if not isinstance(self._value, str):
+      self.raise_invalid('is not a string')
+    return cast(Validator[str], self)
+
+  def to_list(self) -> Validator[list[Any]]:
+    """Coerce the current value to a list."""
+    if not isinstance(self._value, list):
+      self.raise_invalid('is not a list')
+    return cast(Validator[list[Any]], self)
+
+  def items(self) -> Iterator[Validator[T]]:
+    """
+    Get an iterator over the current list value's items. Each item is wrapped in
+    the appropriate validator to continue validating the JSON data. If the
+    current value is not a list, this method raises an assertion error.
+    """
+    value = self._value
+    assert isinstance(value, list)
+    for index, item in enumerate(value):
+      yield Validator(item, key=index, parent=self)
+
+  def to_object(
+    self,
+    valid_keys: set[str] = None,
+    singleton: bool = False
+  ) -> Validator[dict[str,Any]]:
+    """
+    Coerce the current value to an object. If ``valid_keys`` are given, this
+    method validates the object's fields against the given field names. If
+    ``singleton`` is ``True``, the object must have exactly one field.
+    """
+    if not isinstance(self._value, dict):
+      self.raise_invalid('is not an object')
+    keys = cast(dict[str,Any], self._value).keys()
+    if singleton and len(keys) != 1:
+      self.raise_invalid('is not an object with a single field')
+    if valid_keys is not None:
+      for key in keys:
+        if key not in valid_keys:
+          self.raise_invalid(f'contains unexpected field {key}')
+    return cast(Validator[dict[str,Any]], self)
+
+  def __getitem__(self, key: KeyT) -> Validator[T]:
+    """
+    Index the current value with the given key to create a new child validator.
+    The given key becomes the new validator's key and the result of the indexing
+    operation becomes the new validator's value. The new validator's parent is
+    this validator.
+
+    If the current value is a list, the key must be a positive, in-bounds
+    integer. This method raises a ``TypeError`` for non-integer keys and an
+    ``IndexError`` for out-of-bounds integer keys. If the current value is an
+    object, the key must be a string naming an existing field. This method
+    raises a ``TypeError`` for non-string keys and a ``KeyError`` for keys
+    naming non-existent fields. This method also raises a ``TypeError`` if the
+    current value is neither a list nor an object.
+    """
+    value = self._value
+    if isinstance(value, list):
+      if not isinstance(key, int):
+        raise TypeError(f'Non-integer key "{key}" cannot index list')
+      elif key < 0:
+        raise IndexError(f'List index {key} is negative')
+      elif key >= len(value):
+        raise IndexError(f'List index {key} >= length {len(value)}')
+      else:
+        return Validator(value[key], key=key, parent=self)
+    elif isinstance(value, dict):
+      if not isinstance(key, str):
+        raise TypeError(f'Non-string key "{key}" cannot index dict')
+      elif key not in value:
+        raise KeyError(f'Value "{value}" does not have field "{key}"')
+      else:
+        return Validator(value[key], key=key, parent=self)
+    else:
+      raise TypeError(f'Scalar value "{value}" cannot be indexed')
