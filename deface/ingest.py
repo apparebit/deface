22ac6b802d27bbc28cab0a033a04f141c09279d4
Robert Grimm <apparebit@gmail.com>
2021-08-30 00:07:17 -0400

    Rewrite to use Validator instead of JsonData

--- deface/ingest.py
+++ deface/ingest.py
@@ -15,78 +15,82 @@
 import dataclasses
 
 from deface.error import DefaceError, MergeError, ValidationError
-from deface.json_data import JsonData
 from deface.model import (
   Comment, Event, ExternalContext, Location, Media, MediaType, Post, PostHistory
 )
+from deface.validator import Validator
 
 _COMMENT_KEYS: set[str] = { 'author', 'comment', 'timestamp' }
 
-def ingest_comment(data: JsonData) -> Comment:
+def ingest_comment(data: Validator) -> Comment:
   """
   Ingest the current JSON data value as a comment.
   """
-  fields = data.to_object(_COMMENT_KEYS)
-  data['author'].to_string()
-  data['comment'].to_string()
-  data['timestamp'].to_integer()
-  return Comment(**fields)
+  comment_data = data.to_object(valid_keys=_COMMENT_KEYS)
+  fields = comment_data.value
+  comment_data['author'].to_string()
+  comment_data['comment'].to_string()
+  comment_data['timestamp'].to_integer()
+  return Comment(**fields) # type: ignore
 
 # ------------------------------------------------------------------------------
 
 _EVENT_KEYS = { 'name', 'start_timestamp', 'end_timestamp' }
 
-def ingest_event(data: JsonData) -> Event:
+def ingest_event(data: Validator) -> Event:
   """
   Ingest the current JSON data value as an event.
   """
-  fields = data.to_object(_EVENT_KEYS)
-  data['name'].to_string()
-  data['start_timestamp'].to_integer()
-  data['end_timestamp'].to_integer()
-  return Event(**fields)
+  event_data = data.to_object(valid_keys=_EVENT_KEYS)
+  fields = event_data.value
+  event_data['name'].to_string()
+  event_data['start_timestamp'].to_integer()
+  event_data['end_timestamp'].to_integer()
+  return Event(**fields) # type: ignore
 
 # ------------------------------------------------------------------------------
 
 _EXTERNAL_CONTEXT_KEYS = { 'name', 'source', 'url' }
 
-def ingest_external_context(data: JsonData) -> ExternalContext:
+def ingest_external_context(data: Validator) -> ExternalContext:
   """
   Ingest the current JSON data value as an external context.
   """
-  fields = data.to_object(_EXTERNAL_CONTEXT_KEYS)
-  if 'name' in data.value:
-    data['name'].to_string()
-  if 'source' in data.value:
-    data['source'].to_string()
-  data['url'].to_string()
-  return ExternalContext(**fields)
+  context_data = data.to_object(valid_keys=_EXTERNAL_CONTEXT_KEYS)
+  fields = context_data.value
+  if 'name' in context_data.value:
+    context_data['name'].to_string()
+  if 'source' in context_data.value:
+    context_data['source'].to_string()
+  context_data['url'].to_string()
+  return ExternalContext(**fields) # type: ignore
 
 # ------------------------------------------------------------------------------
 
 _LOCATION_KEYS: set[str] = { 'address', 'coordinate', 'name', 'url' }
 _COORDINATE_KEYS: set[str] = { 'latitude', 'longitude' }
 
-def ingest_location(data: JsonData) -> Location:
+def ingest_location(data: Validator) -> Location:
   """
   Ingest the current JSON data value as a location.
   """
-  data.to_object(_LOCATION_KEYS)
+  location_data = data.to_object(valid_keys=_LOCATION_KEYS)
   fields = {}
-  if 'address' in data.value:
-    fields['address'] = data['address'].to_string()
+  if 'address' in location_data.value:
+    fields['address'] = location_data['address'].to_string().value
 
-  if 'coordinate' in data.value:
-    with data['coordinate']:
-      data.to_object(_COORDINATE_KEYS)
-      fields['latitude'] = data['latitude'].to_float()
-      fields['longitude'] = data['longitude'].to_float()
+  if 'coordinate' in location_data.value:
+    coordinates = location_data['coordinate'].to_object(
+      valid_keys=_COORDINATE_KEYS
+    )
+    fields['latitude'] = float(coordinates['latitude'].to_number().value)
+    fields['longitude'] = float(coordinates['longitude'].to_number().value)
 
-  fields['name'] = data['name'].to_string()
-  if 'url' in data.value:
-    fields['url'] = data['url'].to_string()
+  fields['name'] = location_data['name'].to_string().value
+  if 'url' in location_data.value:
+    fields['url'] = location_data['url'].to_string().value
 
-  return Location(**fields)
+  return Location(**fields) # type: ignore
 
 # ------------------------------------------------------------------------------
 
@@ -102,143 +106,133 @@ _MEDIA_KEYS: set[str] = {
 
 _METADATA_KEYS: set[str] = { 'photo_metadata', 'video_metadata' }
 
-def ingest_media(data: JsonData) -> Media:
+def ingest_media(data: Validator) -> Media:
   """
   Ingest the current JSON data value as media.
   """
-  data.to_object(_MEDIA_KEYS)
+  media_data = data.to_object(valid_keys=_MEDIA_KEYS)
   fields = {}
 
   comments = []
-  if 'comments' in data.value:
-    with data['comments']:
-      for index in data.to_list_range():
-        with data[index]:
-          comments.append(ingest_comment(data))
+  if 'comments' in media_data.value:
+    for comment_data in media_data['comments'].to_list().items():
+      comments.append(ingest_comment(comment_data))
   fields['comments'] = tuple(comments)
 
-  #if 'creation_timestamp' in data.value:
-  fields['creation_timestamp'] = data['creation_timestamp'].to_integer()
+  fields['creation_timestamp'] = (
+    media_data['creation_timestamp'].to_integer().value
+  )
 
-  if 'description' in data.value:
-    fields['description'] = data['description'].to_string()
+  if 'description' in media_data.value:
+    fields['description'] = media_data['description'].to_string().value
 
-  #if 'media_metadata' in data.value:
-  with data['media_metadata']:
-    key = data.to_singleton_object(_METADATA_KEYS)
-    fields['media_type'] = (
-      MediaType.PHOTO
-      if key == 'photo_metadata'
-      else MediaType.VIDEO
+  media_metadata = media_data['media_metadata'].to_object(
+    valid_keys=_METADATA_KEYS, singleton=True
+  )
+  metadata_key = media_metadata.only_key
+  fields['media_type'] = (
+    MediaType.PHOTO
+    if metadata_key == 'photo_metadata'
+    else MediaType.VIDEO
+  )
+  fields['metadata'] = media_metadata[metadata_key].to_object().value
+
+  if 'thumbnail' in media_data.value:
+    thumbnail_data = media_data['thumbnail'].to_object(
+      valid_keys={'uri'}, singleton=True
     )
-    fields['metadata'] = data[key].to_object()
-  #else:
-  #  fields['metadata'] = dict()
+    fields['thumbnail'] = thumbnail_data['uri'].to_string().value
 
-  if 'thumbnail' in data.value:
-    with data['thumbnail']:
-      data.to_singleton_object(['uri'])
-      fields['thumbnail'] = data['uri'].to_string()
+  fields['title'] = media_data['title'].to_string().value
+  fields['uri'] = media_data['uri'].to_string().value
 
-  #if 'title' in data.value:
-  fields['title'] = data['title'].to_string()
-
-  fields['uri'] = data['uri'].to_string()
-
-  # if not 'media_type' in fields:
-  #   fields['media_type'] = (
-  #     MediaType.VIDEO
-  #     if fields['uri'].endswith('.mp4')
-  #     else MediaType.PHOTO
-  #   )
-
-  return Media(**fields)
+  return Media(**fields) # type: ignore
 
 # ------------------------------------------------------------------------------
 
 _ATTACHMENT_KEYS: set[str] = {
-  'event', 'external_context', 'media', 'name', 'place'
+  'event', 'external_context', 'media', 'name', 'place', 'text'
 }
 _DATA_KEYS: set[str] = { 'backdated_timestamp', 'post', 'update_timestamp' }
 _POST_KEYS: set[str] = { 'attachments', 'data', 'tags', 'timestamp', 'title' }
 
 def _handle_attachments(
-  data: JsonData, fields: dict
-) -> tuple[list[Media], list[Location]]:
+  data: Validator, fields: dict
+) -> tuple[list[Media], list[Location], list[str]]:
   all_media = []
   all_places = []
+  all_text = []
 
-  for index in data.to_list_range():
-    with data[index]:
-      data.to_singleton_object(['data'])
-      with data['data']:
-        for index2 in data.to_list_range():
-          with data[index2]:
-            key = data.to_singleton_object(_ATTACHMENT_KEYS)
-            if key == 'media':
-              with data['media']:
-                all_media.append(ingest_media(data))
-            elif key == 'place':
-              with data['place']:
-                a_place = ingest_location(data)
-                did_merge = False
-                for index, another_place in enumerate(all_places):
-                  try:
-                    all_places[index] = a_place.merge(another_place)
-                    did_merge = True
-                  except:
-                    pass
-                if not did_merge:
-                  all_places.append(a_place)
-            elif key in fields:
-              data.signal(f'has repeated values for field "{key}"')
-            elif key == 'event':
-              with data['event']:
-                fields['event'] = ingest_event(data)
-            elif key == 'external_context':
-              with data['external_context']:
-                fields['external_context'] = ingest_external_context(data)
-            else: # key == 'name'
-              fields['name'] = data['name'].to_string()
+  for outer_item in data.to_list().items():
+    outer_data = outer_item.to_object(valid_keys={'data'}, singleton=True)
+    for inner_item in outer_data['data'].to_list().items():
+      inner_data = inner_item.to_object(
+        valid_keys=_ATTACHMENT_KEYS, singleton=True
+      )
+      key = inner_data.only_key
+      if key == 'media':
+        all_media.append(ingest_media(inner_data[key]))
+      elif key == 'place':
+        a_place = ingest_location(inner_data[key])
+        did_merge = False
+        for index, another_place in enumerate(all_places):
+          try:
+            all_places[index] = a_place.merge(another_place)
+            did_merge = True
+            break
+          except MergeError:
+            pass
+        if not did_merge:
+          all_places.append(a_place)
+      elif key == 'text':
+        all_text.append(inner_data['text'].to_string().value)
+      elif key in fields:
+        inner_data.raise_invalid(f'has repeated value for field "{key}"')
+      elif key == 'event':
+        fields['event'] = ingest_event(inner_data[key])
+      elif key == 'external_context':
+        fields['external_context'] = ingest_external_context(inner_data[key])
+      else: # key == 'name'
+        fields['name'] = inner_data[key].to_string().value
 
-  return all_media, all_places
+  return all_media, all_places, all_text
 
-def ingest_post(data: JsonData) -> Post:
+def ingest_post(data: Validator) -> Post:
   """
   Ingest the current JSON data value as a post.
   """
-  data.to_object(_POST_KEYS)
+  post_data = data.to_object(valid_keys=_POST_KEYS)
   fields = {}
 
-  if 'attachments' in data.value:
-    with data['attachments']:
-      all_media, all_places = _handle_attachments(data, fields)
+  if 'attachments' in post_data.value:
+    all_media, all_places, all_text = _handle_attachments(
+      post_data['attachments'], fields
+    )
   else:
     all_media = list()
     all_places = list()
+    all_text = list()
 
-  if 'data' in data.value:
-    with data['data']:
-      for index in data.to_list_range():
-        with data[index]:
-          key = data.to_singleton_object(_DATA_KEYS)
-          if key in fields:
-            data.signal(f'has single, redundant field "{key}"')
-          elif key == 'post':
-            fields['post'] = data['post'].to_string()
-          else:
-            fields[key] = data[key].to_integer()
+  if 'data' in post_data.value:
+    for item in post_data['data'].to_list().items():
+      item_data = item.to_object(valid_keys=_DATA_KEYS, singleton=True)
+      key = item_data.only_key
+      if key in fields:
+        item_data.raise_invalid(f'has redundant field "{key}"')
+      elif key == 'post':
+        fields['post'] = item_data['post'].to_string().value
+      else:
+        fields[key] = item_data[key].to_integer().value
 
   tags = []
-  if 'tags' in data.value:
-    with data['tags']:
-      for index in data.to_list_range():
-        tags.append(data[index].to_string())
+  if 'tags' in post_data.value:
+    for tag_data in post_data['tags'].to_list().items():
+      tags.append(tag_data.to_string().value)
   fields['tags'] = tuple(tags)
 
-  fields['timestamp'] = data['timestamp'].to_integer()
-  if 'title' in data.value:
-    fields['title'] = data['title'].to_string()
+  fields['timestamp'] = post_data['timestamp'].to_integer().value
+  if 'title' in post_data.value:
+    fields['title'] = post_data['title'].to_string().value
 
   # Adjust media descriptions:
   #  1. Remove description from media object, if post body is the same.
@@ -259,25 +253,25 @@ def ingest_post(data: JsonData) -> Post:
         all_media[index] = dataclasses.replace(media, description=None)
   fields['media'] = tuple(all_media)
   fields['place'] = tuple(all_places)
+  fields['text'] = tuple(all_text)
 
-  return Post(**fields)
+  return Post(**fields) # type: ignore
 
 def ingest_into_history(
-  data: JsonData, history: PostHistory
+  data: Validator, history: PostHistory
 ) -> list[DefaceError]:
   """
   Ingest the current JSON data value as list of posts into the given history.
   This function returns a list of ingestion errors.
   """
   errors = []
-  for index in data.to_list_range():
-    with data[index]:
-      try:
-        post = ingest_post(data)
-        history.add(post)
-      except MergeError as err:
-        errors.append(err)
-      except ValidationError as err:
-        err.args = err.args + (data.value,)
-        errors.append(err)
+  for item_data in data.to_list().items():
+    try:
+      post = ingest_post(item_data)
+      history.add(post)
+    except MergeError as err:
+      errors.append(err)
+    except ValidationError as err:
+      err.args = err.args + (data.value,)
+      errors.append(err)
   return errors
