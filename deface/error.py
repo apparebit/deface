0a3ff2a080dad3dac163213170ce0fcc40ab95cf
Robert Grimm <apparebit@gmail.com>
2021-09-08 10:22:58 -0400

    Add __all__ symbol declaration

--- deface/error.py
+++ deface/error.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__all__ = ['DefaceError', 'ValidationError', 'MergeError']
+
 class DefaceError(Exception):
   """
   The base class for errors specific to this package.
