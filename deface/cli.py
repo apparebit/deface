e18ffe3149155051172be831888a0e0632bb27ae
Robert Grimm <apparebit@gmail.com>
2021-09-08 10:24:54 -0400

    Improve arguments and parser, fix output of clean posts, collect and report better statistics

--- deface/cli.py
+++ deface/cli.py
@@ -12,42 +12,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, BooleanOptionalAction
 import sys
 from typing import Any
 
 from deface import __version__
-from deface.json_io import dumps, loads
+from deface.serde import dumps, loads
 from deface.ingest import ingest_into_history
-from deface.logger import Logger
+from deface.logger import Logger, pluralize
 from deface.model import PostHistory, find_simultaneous_posts
 from deface.validator import Validator
+
+__all__ = ['create_parser', 'main']
 
 def create_parser() -> ArgumentParser:
   """Create the argument parser for the deface command line tool."""
   prog = 'deface'
   version = f'{prog} {__version__}'
   description = 'Clean and consolidate posts exported from Facebook.'
-  file_help = 'Path of ``your_posts_n.json`` file extracted from data archive'
+  file_help = 'path of ``your_posts_n.json`` file extracted from data archive'
 
-  parser = ArgumentParser(prog=prog, description=description, add_help=False)
+  parser = ArgumentParser(
+    prog=prog,
+    description=description,
+    # The epilog consists of U+3164, the Hangul filler, to force a blank line.
+    # It's necessary because Python ignores various space characters.
+    epilog='ㅤ',
+    add_help=False,
+  )
+
+  # Tool options
   about = parser.add_argument_group('tool options')
   about.add_argument(
-    '-h', '--help', action='help',
+    '-h', '--help',
+    action='help',
     help='show detailed help message and exit'
   )
-  about.add_argument('-V', '--version', action='version', version=version)
-
-  this_run = parser.add_argument_group('output options')
-  this_run.add_argument(
-    '-f', '--format',
-    nargs=1, default='ndjson', choices=['ndjson', 'json', 'pretty'],
-    help='Set output format to ``ndjson`` for newline-delimited JSON, '
-    '``json`` for regular JSON, or ``pretty`` for nicely indented JSON.'
+  about.add_argument(
+    '-V', '--version',
+    action='version', version=version
   )
 
-  parser.add_argument('filenames', metavar='FILE', nargs='+', help=file_help)
+  # Output options
+  this_run = parser.add_argument_group('output options')
+  this_run.add_argument(
+    '--color',
+    action=BooleanOptionalAction, default=True,
+    help='enable or disable the use of color in the output'
+  )
+  this_run.add_argument(
+    '-f', '--format',
+    choices=['json', 'ndjson', 'pretty', 'none'], default='ndjson',
+    help='export data as plain, newline-delimited, or pretty-printed JSON, '
+    ' or not at all'
+  )
 
+  # Positional arguments
+  parser.add_argument('filenames', metavar='FILE', nargs='+', help=file_help)
   return parser
 
 def main() -> None:
@@ -61,12 +82,15 @@ def main() -> None:
   args = create_parser().parse_args()
 
   # Make errors and warnings appear in line-terminated comments. While they are
-  # printed to standard error instead of standard out, that nonetheless helps to
-  # visually offset them from the JSON output and smart users may just want to
-  # pipe them into the output as well.
-  logger = Logger(prefix = '// ')
+  # printed to standard error instead of standard output, that nonetheless helps
+  # to visually offset them from the JSON output. Besides, some users may just
+  # want to include errors or warnings in the output as well.
+  logger = Logger(prefix = '// ', use_color=args.color)
 
-  # Collect posts.
+  # Iterate over files and collect posts.
+  ingested = 0
+  erroneous = 0
+
   history = PostHistory()
   for filename in args.filenames:
     try:
@@ -75,9 +99,12 @@ def main() -> None:
     except Exception as read_err:
       logger.error(read_err)
       continue
+    if isinstance(json_data, list):
+      ingested += len(json_data)
 
     wrapped_data = Validator[Any](json_data, filename=filename)
     errors = ingest_into_history(wrapped_data, history)
+    erroneous += len(errors)
     for err in errors:
       logger.error(err)
 
@@ -88,32 +115,40 @@ def main() -> None:
   # Warn about multiple posts with the same timestamp. They do happen. But they
   # do happen so rarely that they deserve manual validation.
   simultaneous_posts = find_simultaneous_posts(timeline)
-  for index_range in simultaneous_posts:
-    post = timeline[index_range.start]
-    count = index_range.stop - index_range.start
-    logger.warn(f'There are {count} posts with timestamp {post.timestamp}')
+  for timeline_range in simultaneous_posts:
+    count = timeline_range.stop - timeline_range.start
+    posts = [timeline[index] for index in timeline_range]
+    logger.warn(
+      f'There are {count} posts with timestamp {posts[0].timestamp}',
+      *posts
+    )
 
   # Emit the timeline of posts.
-  if args.format != 'ndjson':
+  if args.format in ['json', 'pretty']:
     sys.stdout.write('[\n')
 
-  for index, post in enumerate(timeline):
-    if args.format != 'pretty':
-      sys.stdout.write(dumps(post))
-    else:
-      sys.stdout.write(dumps(post, indent=2))
-    if args.format != 'ndjson' and index < timeline_length - 1:
-      sys.stdout.write(',\n')
-    else:
-      sys.stdout.write('\n')
+  if args.format != 'none':
+    for index, post in enumerate(timeline):
+      if args.format != 'pretty':
+        sys.stdout.write(dumps(post))
+      else:
+        sys.stdout.write(dumps(post, indent=2))
+      if args.format != 'ndjson' and index < timeline_length - 1:
+        sys.stdout.write(',\n')
+      else:
+        sys.stdout.write('\n')
 
-  if args.format != 'ndjson':
+  if args.format in ['json', 'pretty']:
     sys.stdout.write(']\n')
   sys.stdout.flush()
 
   # Sign off.
-  sign_off = f'Ingested {timeline_length} posts'
-  if logger.error_count > 0:
-    sign_off += f' while also encountering {logger.error_count} errors'
-  sign_off += '.'
+  filecount = len(args.filenames)
+  sign_off = (
+    f'Created {timeline_length} clean {pluralize(timeline_length, "post")} '
+    f'from {ingested} raw {pluralize(ingested, "post")} '
+  )
+  if erroneous > 0:
+    sign_off += f'including {erroneous} malformed {pluralize(erroneous, "one")} '
+  sign_off += f'from {filecount} input {pluralize(filecount, "file")}.'
   logger.done(sign_off)
